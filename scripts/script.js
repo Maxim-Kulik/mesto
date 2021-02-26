b09e8c262a2eb5f24eeb16f139480cf94ee1421f
Maxim Kulik <kulixft.205@gmail.com>
2021-02-26 18:48:36 +0900

    Исправил ошибочки

--- scripts/script.js
+++ scripts/script.js
@@ -4,28 +4,29 @@ let closeButton = document.querySelector('.close-button');
 let formButton = document.querySelector('.form__button');
 let formName = document.querySelector('.profile__name');
 let formStatus = document.querySelector('.profile__status');
-let inputName = document.querySelector('.form__input_name');
-let inputStatus = document.querySelector('.form__input_status');
+let inputName = document.querySelector('.form__input_input-name_name');
+let inputStatus = document.querySelector('.form__input_input-name_status');
 
 function popUpActive() {
-formOverlay.classList.remove('overlay_none');
-
+inputName.value = formName.textContent;
+inputStatus.value = formStatus.textContent;
+formOverlay.classList.add('overlay_none');
 }
 editButton.addEventListener('click', popUpActive);
 
 function popUpClose() {
-  formOverlay.classList.add('overlay_none');
+  formOverlay.classList.remove('overlay_none');
 }
 closeButton.addEventListener('click', popUpClose);
 
 
 
-function editSave() {
-formName.textContent = `${inputName.value}`;
-formStatus.textContent = `${inputStatus.value}`;
-popUpClose()
-inputName.value.textContent = `${formName}`;
-inputStatus.value.textContent = `${formStatus}`;
+function editSave(evt) {
+formName.textContent = inputName.value;
+formStatus.textContent = inputStatus.value;
+popUpClose();
+evt.preventDefault();
 }
-formButton.addEventListener('click', editSave);
+let formProfile = document.querySelector('.form__profile');
+formProfile.addEventListener('submit', editSave);
 
