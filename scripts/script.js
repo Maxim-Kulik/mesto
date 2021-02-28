3eff68a2850328bdcb93a814817554992c8cee5d
Maxim Kulik <kulixft.205@gmail.com>
2021-03-01 01:26:03 +0900

    p4

--- scripts/script.js
+++ scripts/script.js
@@ -6,21 +6,16 @@ let formName = document.querySelector('.profile__name');
 let formStatus = document.querySelector('.profile__status');
 let inputName = document.querySelector('.form__input_input-name_name');
 let inputStatus = document.querySelector('.form__input_input-name_status');
-
 function popUpActive() {
 inputName.value = formName.textContent;
 inputStatus.value = formStatus.textContent;
 formOverlay.classList.add('overlay_none');
 }
 editButton.addEventListener('click', popUpActive);
-
 function popUpClose() {
   formOverlay.classList.remove('overlay_none');
 }
 closeButton.addEventListener('click', popUpClose);
-
-
-
 function editSave(evt) {
 formName.textContent = inputName.value;
 formStatus.textContent = inputStatus.value;
@@ -29,4 +24,3 @@ evt.preventDefault();
 }
 let formProfile = document.querySelector('.form__profile');
 formProfile.addEventListener('submit', editSave);
-
