bdf1edaa38454f184f11553786432508669fce43
Maxim Kulik <kulixft.205@gmail.com>
2021-03-02 02:37:16 +0900

    готово

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,25 @@
 let formOverlay = document.querySelector('.overlay');
 let editButton = document.querySelector('.edit-button');
 let closeButton = document.querySelector('.close-button');
-let formButton = document.querySelector('.form__button');
 let formName = document.querySelector('.profile__name');
 let formStatus = document.querySelector('.profile__status');
 let inputName = document.querySelector('.form__input_input-name_name');
 let inputStatus = document.querySelector('.form__input_input-name_status');
+let formProfile = document.querySelector('.form__profile');
 function popUpActive() {
 inputName.value = formName.textContent;
 inputStatus.value = formStatus.textContent;
-formOverlay.classList.add('overlay_none');
+formOverlay.classList.add('overlay_open');
 }
 editButton.addEventListener('click', popUpActive);
 function popUpClose() {
-  formOverlay.classList.remove('overlay_none');
+  formOverlay.classList.remove('overlay_open');
 }
 closeButton.addEventListener('click', popUpClose);
 function editSave(evt) {
+evt.preventDefault();
 formName.textContent = inputName.value;
 formStatus.textContent = inputStatus.value;
 popUpClose();
-evt.preventDefault();
 }
-let formProfile = document.querySelector('.form__profile');
 formProfile.addEventListener('submit', editSave);
